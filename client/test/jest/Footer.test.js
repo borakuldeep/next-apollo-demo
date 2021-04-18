a786504da8d510ee413aabaa53590e6c2515ae9b
Kuldeep <36533806+borakuldeep@users.noreply.github.com>
2021-04-18 19:27:23 +0300

    Update Footer.test.js

--- client/test/jest/Footer.test.js
+++ client/test/jest/Footer.test.js
@@ -3,7 +3,7 @@ import Footer from '../../components/Footer';
 
 test('check footer rendered', () => {
   render(<Footer />)
-  const footerText = 'Copyright 2021 Next-Apollo';
+  const footerText = 'Copyright 2021 Next-Apollo. Dev - Kuldeep Bora';
 
   expect(screen.getByText(footerText)).toBeInTheDocument();
 })

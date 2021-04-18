f9d0b2f8eef878bbc2edadf8d08c7bad8f564924
Kuldeep <36533806+borakuldeep@users.noreply.github.com>
2021-04-18 09:49:24 +0300

    release version 1
    add dockerfile / remove unused packages

--- server/schema.js
+++ server/schema.js
@@ -1,29 +1,6 @@
-// const {
-//   graphql,
-//   GraphQLSchema,
-//   GraphQLObjectType,
-//   GraphQLString
-// } = require('graphql')
-// const faker = require('faker')
-
-// module.exports = new GraphQLSchema({
-//   query: new GraphQLObjectType({
-//     name: 'RootQueryType',
-//     fields: {
-//       name: {
-//         type: GraphQLString,
-//         resolve() {
-//           return faker.name.findName()
-//         }
-//       }
-//     }
-//   })
-// })
-
 const { gql } = require('apollo-server');
 
 const typeDefs = gql`
-  # Your schema will go here
   type User {
   name: String!
   gender: String!
